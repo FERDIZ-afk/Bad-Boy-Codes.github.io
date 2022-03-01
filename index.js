e02ee2e9b98a90d0a564492e2099554e65e22560
Bad Boy <58816669+Bad-Boy-Codes@users.noreply.github.com>
2022-03-01 16:14:03 +0000

    eweeweww

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-let app=require("express")();app.get("/",(e,p)=>p.sendFile(require("path").resolve("index.html"))),app.listen(3e3),console.log("Website Ready!");
+require("express")().get("/",(e,p)=>p.sendFile(require("path").resolve("index.html"))).listen(3e3),console.log("Website Ready!");
